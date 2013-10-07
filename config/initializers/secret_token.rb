1130aad4c1f1bceab287416c078b1f81f05ff8d0
kevlarmusik <kevlar.eq@gmail.com>
2013-10-07 02:30:01 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Poogames::Application.config.secret_key_base = 'aef68853addf2b9a148d3a85bae70e980947cb879e6bb113517249fa4c250b07f192f3a17fda7675280845eea0c7cfbe7bb1636df0e354150fbb2b8b92493edf'
